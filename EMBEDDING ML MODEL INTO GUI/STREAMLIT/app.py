3769a37614b592c921c369d1d006c8160d9f3fb2
Elvis DARKO <elvis_darko@outlook.com>
2023-06-11 11:11:25 +0000

    title added to app

--- EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
+++ EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
@@ -1,6 +1,13 @@
+# Import Streamlit
 import streamlit as st
-# import pandas as pd
-# import numpy as np
+
+# import pandas
+import pandas as pd
+
+# import numpy 
+import numpy as np
 # import matplotlib.pyplot as plt
 # import seaborn as sns
+
+st.title("STORE FAVORITA SALES FORECASTstreamlit run")
 
