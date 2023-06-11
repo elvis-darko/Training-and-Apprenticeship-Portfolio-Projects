ea073f129ed7cfd9bdbb86c9f2c1edf646c4dfc9
Elvis DARKO <elvis_darko@outlook.com>
2023-06-11 13:56:13 +0000

    staff_id section added to app

--- EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
+++ EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 # import numpy 
 import numpy as np
+
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 
@@ -16,6 +17,9 @@ st.subheader("Peroidic Sales Predictions")
 # Enter info with streamlit form
 with st.form("prediction_form") as form:
     name = st.text_input(label="Please enter you full name")
+    role = st.text_input(label="Please enter your role")
+    staff_id = st.number_input(label="Please enter your staff ID")
+    submit = st.form_submit_button()
   
 
 print(f"[info] My name is : {name}")
