836b55850718f3683eedfcc98f340102181d8476
Elvis DARKO <elvis_darko@outlook.com>
2023-06-11 11:47:13 +0000

    name bar added

--- EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
+++ EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
@@ -9,7 +9,11 @@ import numpy as np
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 
+# Enter the title and sub-header of app
 st.title("STORE FAVORITA SALES FORECAST")
 st.subheader("Peroidic Sales")
+
+# Ener name of officer
+name = st.text_input(label="Please enter you full name")
 
 
