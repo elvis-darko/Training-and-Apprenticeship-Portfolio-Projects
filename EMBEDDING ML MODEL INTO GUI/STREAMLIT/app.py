e7373f775fa796eb23aa649e28d8cc84a019c9fe
Elvis DARKO <elvis_darko@outlook.com>
2023-06-12 04:40:07 +0000

    packages added

--- EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
+++ EMBEDDING ML MODEL INTO GUI/STREAMLIT/app.py
@@ -1,28 +1,27 @@
-# Import Streamlit
+# Import packages
 import streamlit as st
-
-# import pandas
 import pandas as pd
-
-# import numpy 
 import numpy as np
+import pickle
+from PIL import Image
 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
 
-# Enter the title and sub-header of app
-st.title("STORE FAVORITA SALES FORECAST")
-st.subheader("Peroidic Sales Predictions")
 
-# Enter info with streamlit form
-with st.form("prediction_form") as form:
-    name = st.text_input(label="Please enter you full name")
-    role = st.text_input(label="Please enter your role")
-    staff_id = st.number_input(label="Please enter your staff ID")
-    submit = st.form_submit_button()
+
+
+# # Enter the title and sub-header of app
+# st.title("STORE FAVORITA SALES FORECAST")
+# st.subheader("Peroidic Sales Predictions")
+
+# # Enter info with streamlit form
+# with st.form("prediction_form") as form:
+#     name = st.text_input(label="Please enter you full name")
+#     role = st.text_input(label="Please enter your role")
+#     staff_id = st.number_input(label="Please enter your staff ID")
+#     submit = st.form_submit_button()
   
 
-print(f"[info] My name is : {name}")
+# print(f"[info] My name is : {name}")
 
 
 
